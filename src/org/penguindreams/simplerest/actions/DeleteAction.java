32140944312cce80fc56026a6fff66e89156aa7f
U-PNG\skhanna <skhanna@DT2226.pngmail.com>
2010-04-23 17:04:08 -0400

    added actions

---
+++ src/org/penguindreams/simplerest/actions/DeleteAction.java
@@ -0,0 +1,14 @@
+package org.penguindreams.simplerest.actions;
+
+import org.penguindreams.simplerest.service.ServiceRequest;
+import org.penguindreams.simplerest.service.ServiceResponse;
+
+public class DeleteAction extends AbstractServiceAction {
+
+	@Override
+	public ServiceResponse invoke(ServiceRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
