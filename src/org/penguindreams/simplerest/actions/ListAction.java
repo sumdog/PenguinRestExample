9ae036e299e7c35869dc78c8a195a532036a4cc5
Sumit Khanna <sum.notify@gmail.com>
2010-05-20 17:00:29 -0400

    Deployment now works and have the basic hello world stuff up and running Added AOP jar files

---
+++ src/org/penguindreams/simplerest/actions/ListAction.java
@@ -0,0 +1,13 @@
+package org.penguindreams.simplerest.actions;
+
+import org.penguindreams.simplerest.service.ServiceRequest;
+import org.penguindreams.simplerest.service.ServiceResponse;
+
+public class ListAction extends AbstractServiceAction {
+
+	@Override
+	public ServiceResponse invoke(ServiceRequest request) {
+		return null;
+	}
+
+}
